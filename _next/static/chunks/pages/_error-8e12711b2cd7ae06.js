7b986d54f8cd6f463930470fdfcf0bf41c267865
AlanTapia <44512347+soyalantapia@users.noreply.github.com>
2026-05-27 10:17:48 -0300

    deploy: 30 audit finales aplicados

---
+++ _next/static/chunks/pages/_error-8e12711b2cd7ae06.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2731],{2873:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(51114)}])}},_=>{_.O(0,[636,6593,8792],()=>_(_.s=2873)),_N_E=_.O()}]);
